3bc05095ad4e55386fc97bebb326c2300f9f1e19
Ceratosenzaf <ceratosenzaf@gmail.com>
2025-08-19 09:46:06 +0200

    fix: build.rs rerun if changed

--- crates/spec-trait-macro/build.rs
+++ crates/spec-trait-macro/build.rs
@@ -6,5 +6,6 @@ fn main() {
     fs::write(&get_cache_path(), "{}").expect("Failed to write file cache");
 
     println!("cargo:warning=Running build.rs at {}", Local::now().to_rfc3339());
-    println!("cargo::rerun-if-changed=/dev/null");
+    println!("cargo:rerun-if-changed=src");
+    println!("cargo:rerun-if-changed={}", get_cache_path().to_string_lossy());
 }
