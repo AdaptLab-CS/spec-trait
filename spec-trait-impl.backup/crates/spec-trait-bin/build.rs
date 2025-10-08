2ca7ba64653bbea5d6c33128a34c98c41f36f3ae
Ceratosenzaf <ceratosenzaf@gmail.com>
2025-10-08 10:25:25 +0200

    chore: removed backup

--- spec-trait-impl.backup/crates/spec-trait-bin/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    spec_trait_order::handle_order();
-}
--- spec-trait-impl.backup/crates/spec-trait-utils/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod cache;
-pub mod conditions;
-pub mod conversions;
-pub mod env;
-pub mod impls;
-pub mod parsing;
-mod specialize;
-pub mod traits;
-pub mod types;
